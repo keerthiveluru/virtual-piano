a6916bf9d2cc7b295d1f64af59d66a5e06e45743
keerthiveluru <85473954+keerthiveluru@users.noreply.github.com>
2021-07-10 16:46:52 +0530

    Add files via upload

---
+++ piano.js
@@ -0,0 +1,117 @@
+function changecolor(){
+    var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor="rgb("+r+","+g+","+b+")"
+              document.getElementById("white1").style.background=mycolor
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("black1").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white2").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("black2").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white3").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white4").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("black3").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white5").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("black4").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white6").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("black5").style.background=mycolor1
+
+              var r=Math.floor(Math.random()*256);
+              var g=Math.floor(Math.random()*256);
+              var b=Math.floor(Math.random()*256);
+              var mycolor1="rgb("+r+","+g+","+b+")"
+              document.getElementById("white7").style.background=mycolor1
+              
+              
+              
+
+
+
+             
+    
+    }
+  var A=new Audio()
+  A.src="notes/notes_A.mp3";
+  
+
+  var Ab=new Audio()
+  Ab.src="notes/notes_Ab.mp3";
+
+  var B=new Audio()
+  B.src="notes/notes_B.mp3";
+
+  var Bb=new Audio()
+  Bb.src="notes/notes_Bb.mp3";
+
+  var C=new Audio()
+  C.src="notes/notes_C.mp3";
+
+  var D=new Audio()
+  D.src="notes/notes_D.mp3";
+
+  var Db=new Audio()
+  Db.src="notes/notes_Db.mp3";
+
+  var E=new Audio()
+  E.src="notes/notes_E.mp3";
+
+  var Eb=new Audio()
+  Eb.src="notes/notes_Eb.mp3";
+
+  var F=new Audio()
+  F.src="notes/notes_F.mp3";
+
+  var G=new Audio()
+  G.src="notes/notes_G.mp3";
+
+  var Gb=new Audio()
+  Gb.src="notes/notes_Gb.mp3";
